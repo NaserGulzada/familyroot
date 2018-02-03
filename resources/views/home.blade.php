9df4349ad57b454483172ef6de27bd0941eed02c
Naser <mngafghan@gmail.com>
2018-02-03 22:41:26 +0430

    Only a few welcome message set, started development

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<h1>Family Root Tree Application</h1>
+<h3>Dear Developers</h3>
+<p>The development of the family tree database is under the development and will be uploaded soon.</p>
+<p>Any contributers to this projet is welcome.</p>
