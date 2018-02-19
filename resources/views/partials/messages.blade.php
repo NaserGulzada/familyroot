3e4a2273b0844919861eb28083c89769c410c2f7
Naser <mngafghan@gmail.com>
2018-02-19 19:28:23 +0430

    Last change with messages to show

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -10,6 +10,17 @@
      {{Session::get('success')}}
  </div>
 @endif
+<!--
+@if(count($errors)>0)
+  <div class="alert alert-danger" role="alert">
+    <ul>
+    @foreach($errors->all() as $error)
+     <li>{{$error}}</li>
+    @endforeach
+    </ul>
+</div>
+@endif
+-->
 @if(Session::has('error'))
  <div class="alert alert-error">
      {{Session::get('error')}}
